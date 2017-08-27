c2c972850fccbbff039376372513c673eeac1f3d
Bhupali <bhupali.chiplunkar@gmail.com>
2017-08-27 14:23:47 +0530

    - Fix deconstructor issue.

--- src/components/tableView.js
+++ src/components/tableView.js
@@ -3,11 +3,12 @@ import TimeAgo from 'react-timeago';
 
 class TableView extends Component{
     renderRows (ticker){
+        var index = `${ticker.name}-${ticker.price}`;
         return (
-            <tr key={ticker.name} className={ticker.class}>
+            <tr key={index} className={ticker.class}>
                 <td className="data-cell">{ticker.name}</td>
-                <td className="data-cell">{ticker.roundedOffPrice}</td>
-                <td className="data-cell"><TimeAgo datetime={ticker.updatedAt} /></td>
+                <td className="data-cell">{ticker.price}</td>
+                <td className="data-cell"><TimeAgo date={ticker.updatedAt} /></td>
             </tr>
         )
     }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,34 +7,34 @@ const CLASS_CONSTANTS = {
 }
 
 export const formatIncomingData = (data) => {
-    const modifiedData = data.reduce((final, curr) =>{
-        const [name,price,] = curr;
-        final.push({name,price, updatedAt : new Date()})
+    let modifiedData = data.reduce((final, curr) =>{
+        let [name,price,] = curr;
+            final.push({name,price, updatedAt : new Date().getTime()})
         return final;
     }, [])
     return modifiedData
 }
 
 export const updateListData = (newData, oldData) => {
-    return Array.from(newData).reduce((updatedArr, currentElement) => {
-        const updatedAt = new Date();
-        const [name,price] = currentElement;
-        const newState = oldData.slice(0);
-        let elemIndex = newState.findIndex(x => x.name === name)
-        if(elemIndex===-1){
-            newState.push({
-                name,
-                price,
-                updatedAt,
-                class : CLASS_CONSTANTS.NEW,
-                dataseries : [].push(price)
-            })
-        } else {
-            newState[elemIndex].class = newState[elemIndex].price > price ? CLASS_CONSTANTS.DECREASED : newState[elemIndex].price < price? CLASS_CONSTANTS.INCREASED :CLASS_CONSTANTS.CONSTANT;
-            newState[elemIndex].price = price;
-            newState[elemIndex].updatedAt = updatedAt;
-            newState[elemIndex].dataseries.push(price);
-        }
+        let newState = Object.assign([],oldData);
+        let updatedAt = new Date().getTime();
+        newData.forEach(function(elem) {
+            let {name,price} = elem;
+            let elemIndex = newState.findIndex(x => x.name === name);
+            if(elemIndex===-1){
+                newState.push({
+                    name,
+                    price,
+                    updatedAt,
+                    class : CLASS_CONSTANTS.NEW,
+                    dataseries : [].push(price)
+                })
+            } else {
+                newState[elemIndex].class = newState[elemIndex].price > price ? CLASS_CONSTANTS.DECREASED : newState[elemIndex].price < price? CLASS_CONSTANTS.INCREASED :CLASS_CONSTANTS.CONSTANT;
+                newState[elemIndex].price = price;
+                newState[elemIndex].updatedAt = updatedAt;
+                newState[elemIndex].dataseries = [...newState[elemIndex].dataseries,price];
+            }
+        }, this);
         return newState;
-    }, oldData)  
 }
